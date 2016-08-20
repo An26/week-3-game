4fe3dfc58537d4c38b5f0adb1428f686f7b2a757
An Huynh <An Huynh>
2016-08-20 15:15:51 -0500

    finished!

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -4,26 +4,23 @@ var losses = 0;
 var guessChances = 12;
 var wrongGuess = [];
 var spacesArray = [];
-//word bank
-var choiceWords = ["rick", "morty", "jeez" , "summer", "jerry","beth", "meseeks", "squanchy", "plutonians", "snuffles", "snowball", "cromulons", "zigerions", "unity", "gazorpazorp", "hamurai"];
+var choiceWords = ["rick", "morty", "jeez", "summer", "jerry", "beth", "meseeks", "squanchy", "plutonians", "snuffles", "snowball", "cromulons", "unity", "gazorpazorp"];
 
 //possible key guesses
- var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
- //computer picks random word
- var computerGuess = choiceWords[Math.floor(Math.random()*choiceWords.length)];
- console.log(computerGuess);
+//computer picks random word
+var computerGuess = choiceWords[Math.floor(Math.random() * choiceWords.length)];
+console.log(computerGuess);
 
- var computerGuessArray = computerGuess.split('');
- console.log(computerGuessArray);
+var computerGuessArray = computerGuess.split('');
+console.log(computerGuessArray);
 
-//document.adEventListener ("DOMContentLoaded", function(){
-//});
 
 //splice method changes _ to letters MGM
 //----------------functions------------------------------
-function reset () {
-    computerGuess = choiceWords[Math.floor(Math.random()*choiceWords.length)];
+function reset() {
+    computerGuess = choiceWords[Math.floor(Math.random() * choiceWords.length)];
     computerGuessArray = computerGuess.split('');
     spacesArray = [];
     spaceControl();
@@ -31,53 +28,45 @@ function reset () {
     wrongGuess = [];
     console.log("this is my reset");
 
-     var html =
-     "<p>guess this word: " + spacesArray + "</p>";
-    // //"<p>guess word: " + spaceControl(userGuess) + "</p>" +
-    // "<p>wins: " + wins +"</p>" + 
-    // "<p>losses: " + losses + "</p>" +
-    // "<p>guesses: " + guessChances + "</p>" +
-    // "<p>letters guessed: " + wrongGuess + "</p>";
-    // // Placing the html into the game 
-     document.querySelector('.hangman').innerHTML = html; //prints out var html
-
-    
 }
 
 //prints out blank spaces of computer guess
-function spaceControl (){
-    for (var i = 0; i < computerGuess.length; i++){
-        spacesArray.push('_'); 
+function spaceControl() {
+    for (var i = 0; i < computerGuess.length; i++) {
+        spacesArray.push('_');
         console.log("guess this word " + spacesArray);
     }
 }
 
 //check letter is in array
-    function checkLetter(userGuess){
-        if(computerGuessArray.indexOf(userGuess) !== -1){
-            for (var i = 0; i < computerGuessArray.length; i++){
-                if (userGuess === computerGuessArray[i]) {
-                    spacesArray.splice(i,1,userGuess);
-                    console.log("spacesArray: " + spacesArray)
-                    console.log("userGuess: " + userGuess);
-                }
+function checkLetter(userGuess) {
+    if (computerGuessArray.indexOf(userGuess) !== -1) {
+        for (var i = 0; i < computerGuessArray.length; i++) {
+            if (userGuess === computerGuessArray[i]) {
+                spacesArray.splice(i, 1, userGuess);
+                console.log("spacesArray: " + spacesArray)
+                console.log("userGuess: " + userGuess);
             }
-        } else if (guessChances < 1){
-            //console.log("guess chances = " + guessChances);
-            console.log("that\'s it! press any letter to play again.");
-            losses++;
-            reset();
-        }  else if (guessChances < 12 && spacesArray.indexOf('_') === -1){
-            wins++;
-            reset();
-        }  else {
+        }
+    } else if (guessChances < 1) {
+        //console.log("guess chances = " + guessChances);
+        console.log("that\'s it! press any letter to play again.");
+        alert("nice try! The word to save Morty was " + computerGuess + "!!!");
+        losses++;
+        reset();
+
+    } else if ((guessChances < 12) && (spacesArray.indexOf('_') === -1)) {
+        alert("Nice you saved a Morty!!");
+        wins++;
+        reset();
+    } else {
         //not in word
         wrongGuess.push(userGuess);
         guessChances--;
         console.log(wrongGuess + " is not a right letter");
         console.log(guessChances);
-        }
     }
+}
 
 
 //-----------------method/execution-------------------------------
@@ -93,31 +82,19 @@ document.onkeyup = function(event) {
     console.log("user guess: " + userGuess);
     console.log("computer guess: " + computerGuess);
 
-  
+
     checkLetter(userGuess);
 
 
     var html =
-    "<p>guess this word: " + spacesArray + "</p>" +
-    //"<p>guess word: " + spaceControl(userGuess) + "</p>" +
-    "<p>wins: " + wins +"</p>" + 
-    "<p>losses: " + losses + "</p>" +
-    "<p>guesses: " + guessChances + "</p>" +
-    "<p>letters guessed: " + wrongGuess + "</p>";
+        "<p>guess this word: " + spacesArray + "</p>" +
+        //"<p>guess word: " + spaceControl(userGuess) + "</p>" +
+        "<p>wins: " + wins + "</p>" +
+        "<p>losses: " + losses + "</p>" +
+        "<p>guesses: " + guessChances + "</p>" +
+        "<p>letters guessed: " + wrongGuess + "</p>";
     // Placing the html into the game 
     document.querySelector('.hangman').innerHTML = html; //prints out var html
 
 
 }
-
-    //get user's guess
-    //get computer's guess from word bank
-    //loop: see if user's guess is in computer's guess (compare)
-    //if yes, print letter in the line for the word (what to do if it's there more than once?) (how to print it out in the line of dashes in order?)
-    //how to print this out in the line???
-    //if no, print out no and letter in wrongGuess array
-    //loop back again
-    //if user guess # of times is < set limit of guesses (12) then wins ++, print it out (how do i do a counter for that?).
-    //if user guess #3 of trys is > set limit of guesses then loses ++, print loses out.
-
-    // </script>
